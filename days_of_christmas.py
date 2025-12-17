ffbf4165507a7c124791c786e378aeef279288af
Agatha00133 <agathansofor1@gmail.com>
2025-12-17 16:30:04 +0100

    Days of christmas

---
+++ days_of_christmas.py
@@ -0,0 +1,62 @@
+for day in range(1, 13): 
+
+    match day:
+        case 1:
+                print("first day of christmas")
+        case 2:
+                print("secord day of christmas")
+        case 3:
+                print("third day of christmas")
+        case 4:
+                print("fourth day of christmas")
+        case 5: 
+                print("fifth day of chirstmas") 
+        case 6: 
+                print("sixth day of chirstmas")
+        case 7:
+                print("seventh day of chirstmas")
+        case 8:
+                print("eighth day of christmas")
+        case 9:
+                print("ninth day of christmas")
+        case 10: 
+                print("tenth day of christmas")
+        case 11:
+                print("eleventh day of christmas")
+        case 12: 
+                print("twelfth day of christmas")
+    print("my true love said to me")
+
+    for gifts in range(day,0,-1):
+
+         match gifts: 
+             case 12:
+                print ("twelve drummers drumming") 
+             case 11:
+                print ("eleven pipers piping")
+             case 10:
+                print ("ten lords a-leaping")
+             case 9:
+                print ("nine ladies dancing")     
+             case 8:
+                print ("eight maids a- milking")
+             case 7:
+               print ("seven swans a - swimming")
+             case 6:
+               print("six geese a- laying")
+             case 5: 
+               print ("five gold rings")
+             case 4:
+               print("four calling birds")
+             case 3:
+                print("three french hens")
+             case 2:
+                print ("two turtle doves")
+             case 1:
+               print("A partrige in a pear tree")
+        
+    print()
+
+
+
+        
